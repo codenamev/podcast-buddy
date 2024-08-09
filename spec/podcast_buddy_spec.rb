61caad8e0dde01b1239f916652e2b69b892da539
Valentino Stoll <vstoll@doximity.com>
2024-08-09 09:49:15 -0400

    Tests pass for now

--- spec/podcast_buddy_spec.rb
+++ spec/podcast_buddy_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe PodcastBuddy do
   it "has a version number" do
     expect(PodcastBuddy::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
